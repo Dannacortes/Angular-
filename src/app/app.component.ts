d38ed79e1096ec47853e93f3e72438062277bd2c
Tu Nombre <tuemail@example.com>
2024-09-26 23:53:11 -0500

    La buena fai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,  CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'AirCNC';
